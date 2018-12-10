d5b63d54728b2cf1cd0e9d59272c40453b96c375
bensisa <45767389+bensisa@users.noreply.github.com>
2018-12-10 17:58:10 +0000

    Add files via upload

---
+++ lightning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightning : MonoBehaviour {
+    Collider2D Lightning;
+	// Use this for initialization
+	void Start () {
+        Lightning = this.GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onCol()
+    {
+        Lightning.enabled = true;
+    }
+    public void offCol()
+    {
+        Lightning.enabled = false;
+    }
+    void onTriggerEnter2d(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
